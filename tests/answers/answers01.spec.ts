d26df3e11b7e9eb17761cac50632872955b67694
Bas Dijkstra <bas@ontestautomation.com>
2025-09-16 16:04:57 +0200

    FAIL

--- tests/answers/answers01.spec.ts
+++ tests/answers/answers01.spec.ts
@@ -10,7 +10,7 @@ test('Answers 01', async ({ page }) => {
   // Typing 'secret_sauce' into the text field with placeholder 'Password'.
   // Clicking the button with name 'Login'. See the example for an idea on how to do this.
   await page.getByPlaceholder('Username').fill('standard_user');
-  await page.getByPlaceholder('Password').fill('secret_sauce');
+  await page.getByPlaceholder('Password').fill('secret_sauc');
   await page.getByRole('button', { name: 'Login'}).click();
 
   // Check that the element with text 'Products' is visible
